c7e7d4dfbe3fb9c5c4be4fbb223b43c8f5ff90c5
zhuyingjun-zyj <66345340+zhuyingjun-zyj@users.noreply.github.com>
2024-04-13 16:50:24 +0800

    Update data_clean.py

--- ner_demo/DataProcess/data_clean.py
+++ ner_demo/DataProcess/data_clean.py
@@ -5,6 +5,7 @@ import calendar
 import nltk, re
 from nltk.tokenize import WordPunctTokenizer
 import pandas as pd
+from datetime import datetime
 
 tk = WordPunctTokenizer()  # F:\mylearning\fengkong\stanford-postagger-full-2017-06-09\stanford-postagger-3.8.0.jar
 
@@ -48,19 +49,30 @@ def data2json(origin_file, out_file):
         json.dump(results_final, wf, ensure_ascii=False, indent=2)
 
 
-def get_week(date_str):
-    # print(date_str)
+def get_week_distime(date_str, sub_time):
+
     try:
         year, month, day = map(int, date_str.split(' ')[0].split('-'))
         weekday = calendar.weekday(year, month, day)
         weekday_str = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'][weekday]
-        print(f'{date_str}是{weekday_str}')
-        return weekday_str
-    except:
-        return "Sin fecha"
+
+        start_date = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+        end_date = datetime.strptime(sub_time, "%Y-%m-%d %H:%M:%S")
+        # 计算两个日期之间的天数差
+        dis_time = (end_date - start_date).days
+
+        print(f'{date_str}是{weekday_str} ，，  {date_str} ==>> {sub_time} 时间差 {dis_time}')
+        return weekday_str, dis_time
+
+    except Exception as e:
+        print("有报错 ：" ,e)
+        return "Sin fecha", "Sin fecha"
 
 
-def get_content_clean(contents):
+def get_content_clean(x):
+    # print(x)
+    sub_time = x["f_public_last_submit_time"]
+    contents = x["f_sms_data"]
     contents = eval(contents)
     # contents = json.loads(result["f_sms_data"])
     contentsss = ""
@@ -69,9 +81,10 @@ def get_content_clean(contents):
         oppositePhone = content_one.get("oppositePhone")
         smsTime = content_one.get("smsTime")
         type = content_one.get("type")
-        weekday = get_week(smsTime)
 
-        contentsss = contentsss + str(content) + " , el tiempo " + str(smsTime) + " , semana " + str(weekday) + "\n"
+        weekday, dis_time = get_week_distime(smsTime, sub_time)
+        contentsss = contentsss + str(content) + " , tiempo " + str(smsTime) + " tiempo de entrada  " + str(
+            dis_time) + " días , semana " + str(weekday) + "\n"
 
         # print(contentsss)
     print("===================")
@@ -79,28 +92,20 @@ def get_content_clean(contents):
     return Series([contentsss])
 
 
-def clean_content(origin_file, out_file):
+def clean_content(origin_file, label_file, out_file):
     """
     清洗短信内容
     """
-    df = pd.read_csv(origin_file, sep='/001XG')
-    df["f_sms_data"] = df["f_sms_data"].apply(lambda x: get_content_clean(x))
-    df.to_csv(out_file, index=False, sep='\t', encoding='utf-8')
-    print(df.shape)
+    df = pd.read_csv(origin_file, sep='/001XG')  # /001XG
+    df.dropna(subset=["f_sms_data"], inplace=True)
+    df["f_sms_data"] = df.apply(lambda x: get_content_clean(x), axis=1)
 
-
-def merge_sms_label(content_file, label_file, merge_file):
-    '''
-        将清洗后的短信和标签合并
-    '''
-
-    df_content = pd.read_csv(content_file, sep="\t")
     df_label = pd.read_csv(label_file)
-    # df_label = df_label[["f_public_uid","apply_id",]]
-    df_merge = pd.merge(df_content, df_label, on="f_public_uid", how="inner")
+
+    df_merge = pd.merge(df, df_label, on="f_public_uid", how="inner")
+
+    df_merge.to_csv(out_file, index=False, sep='\t', encoding='utf-8')
     print(df_merge.shape)
-    df_merge.to_csv(merge_file, sep="\t", index=False)
-    return df_merge
 
 
 def fit_tokenizer(merge_file):
@@ -134,7 +139,6 @@ def fit_tokenizer(merge_file):
             geek_line = tk.tokenize(line)
             words_tokens.extend(list(set(geek_line)))
 
-
     words_tokens = list(set(words_tokens))
     # print(f"分词后结果：{words_tokens}")
 
@@ -148,10 +152,9 @@ def fit_tokenizer(merge_file):
 
 
 if __name__ == '__main__':
-    origin_file = r"../data/data/data_sms_old.csv"
-    train_clean_file = r"../data/data/data_sms_old_clean.csv"
+    origin_file = r"F:\mylearning/data/data/data_sms_old.csv"
+    train_merge_file = r"F:\mylearning/data/data/train_data_sms_old_add_días_merge.csv"
     train_label_file = r"../data/data/data_label_old.csv"
-    train_merge_file = r"../data/data/test_clean.csv"
 
     test_origin_file = r"../data/data/data_sms_new.csv"
     test_clean_file = r"../data/data/data_sms_new_clean.csv"
@@ -159,32 +162,29 @@ if __name__ == '__main__':
     test_merge_file = r"../data/data/test_merge.csv"
 
     # data2json(origin_file, out_file)
-    # clean_content(test_origin_file, test_clean_file)
-    # merge_sms_label(test_clean_file, test_label_file, test_merge_file)
+    # pd.read_csv(origin_file, sep='/001XG')[:50].to_csv("F:\mylearning/data/data/data_sms_old_50.csv", sep='\t')
+
+    clean_content(origin_file, train_label_file, train_merge_file)
+
     # fit_tokenizer(merge_file)
     # fit_tokenizer(train_merge_file)
 
-    with open("./tokenizer_sms_test_clean.json", "r", encoding="utf-8") as wf:
-        sms_test_clean = json.load(wf)
-        with open("./tokenizer_sms_train_clean_1.5W_.json", "r", encoding="utf-8") as wf2:
-            sms_train_clean_15w_ = json.load(wf2)
-            with open("./tokenizer_sms_train_clean_15000.json", "r", encoding="utf-8") as wf3:
-                sms_train_clean_15w = json.load(wf3)
-                all_info = {**sms_test_clean, **sms_train_clean_15w_}
-                all_info = {**all_info, **sms_train_clean_15w}
-
-                  # unknown
-                all_info = {k: (v + 3) for k, v in all_info.items()}
-                all_info["[PAD]"] = 0
-                all_info["[END]"] = 1
-                all_info["[UNK]"] = 2
-
-                with open("./tokenizer_sms_all.json", "w", encoding="utf-8") as wf:
-                    json.dump(all_info, wf, indent=2, ensure_ascii=False)
-                print(f"分词：{all_info}")
-                print(f" 字典词量 ：{len(all_info)}")
-
-
-
-
-
+    # with open("./tokenizer_sms_test_clean.json", "r", encoding="utf-8") as wf:
+    #     sms_test_clean = json.load(wf)
+    #     with open("./tokenizer_sms_train_clean_1.5W_.json", "r", encoding="utf-8") as wf2:
+    #         sms_train_clean_15w_ = json.load(wf2)
+    #         with open("./tokenizer_sms_train_clean_15000.json", "r", encoding="utf-8") as wf3:
+    #             sms_train_clean_15w = json.load(wf3)
+    #             all_info = {**sms_test_clean, **sms_train_clean_15w_}
+    #             all_info = {**all_info, **sms_train_clean_15w}
+    #
+    #               # unknown
+    #             all_info = {k: (v + 3) for k, v in all_info.items()}
+    #             all_info["[PAD]"] = 0
+    #             all_info["[END]"] = 1
+    #             all_info["[UNK]"] = 2
+    #
+    #             with open("./tokenizer_sms_all.json", "w", encoding="utf-8") as wf:
+    #                 json.dump(all_info, wf, indent=2, ensure_ascii=False)
+    #             print(f"分词：{all_info}")
+    #             print(f" 字典词量 ：{len(all_info)}")
